a0fc8be49d808932a5367e9b4488dacce458f7d7
msj2002 <amy906504@naver.com>
2023-09-12 13:36:11 +0900

    다시 푸시

--- Week02_MSJ/src/Week02_02.java
+++ Week02_MSJ/src/Week02_02.java
@@ -9,8 +9,12 @@ class Soccer extends Sports {
 		return "경기이름: 축구";	
 	}
 	
-	public void getPlayers() {
-		System.out.println("경기자수: 11");	
+	public int getPlayers() {
+		
+		int playerCount = 11;
+	    System.out.println("경기자수: " + playerCount);
+	    return playerCount;
+			
 	}
 }
 	
@@ -19,6 +23,11 @@ public class Week02_02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Soccer S=new Soccer();
+		System.out.println(S.getName());
+		S.getPlayers();
+	
+		
 
 	}
 
