7765842344baa7d6e2941b724e9ed2d629f673e3
minseo0827 <amy906504@naver.com>
2023-09-19 10:18:43 +0900

    push

---
+++ Week03_MSJ/src/Week03_DVD.java
@@ -0,0 +1,48 @@
+
+interface Players {
+	void play();
+	void stop();
+}
+
+
+interface ExPlayers extends Players{
+	void slow();
+}
+
+class DVDPlayer implements ExPlayers{
+	public void play() {
+		System.out.println("DVD 재생 시작!");
+	}
+	
+	public void stop() {
+		System.out.println("DVD 재생 정지!");
+	}
+	
+	public void slow() {
+		System.out.println("DVD 느린 재생 시작!");
+	}
+}
+public class Week03_DVD {
+	public static void main(String[] args) {
+		DVDPlayer a =new DVDPlayer();
+		Players b= new DVDPlayer();
+		ExPlayers c= new DVDPlayer();
+		
+	System.out.println("DVDPlayer형 변수 a");
+	a.play();
+	a.stop();
+	a.slow();
+	
+	System.out.println("Player형 변수 b");
+	b.play();
+	b.stop();
+	
+	System.out.println("ExPlayer형 변수 c");
+	c.play();
+	c.stop();
+	c.slow();
+	
+
+	}
+
+}
