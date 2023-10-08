a0769a6d5e8da693307d239fc5929004a14b8c04
minseo0827 <amy906504@naver.com>
2023-10-08 22:26:16 +0900

    push

---
+++ Study/src/test97.java
@@ -0,0 +1,11 @@
+public class test97 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("출력되나");
+		System.out.println("뭐야????왜src안올라가나푸시햇는데...이거 수정해도 unstagedchanges에 뜨지도않네");
+				
+	}
+	
+}
