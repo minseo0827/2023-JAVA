518c963128f77f95af7aea7cdeef04156923b37c
minseo0827 <amy906504@naver.com>
2023-10-09 15:25:43 +0900

    p

--- Study/src/test97.java
+++ Study/src/test97.java
@@ -6,6 +6,7 @@ public class test97 {
 		System.out.println("출력되나");
 		System.out.println("뭐야????왜src안올라가나푸시햇는데...이거 수정해도 unstagedchanges에 뜨지도않네");
 		System.out.println("내용을 다시추가하면즉시반영이되나");
+		System.out.println("내용을 다시추가하면즉시반영이되나2");
 				
 	}
 
