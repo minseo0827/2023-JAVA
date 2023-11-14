22619da9a2ec8e757c4185250583480cb2e10a5e
minseo <142555903+minseo0827@users.noreply.github.com>
2023-11-14 10:03:42 +0900

    p

---
+++ Week10_MSJ/src/MyIP.java
@@ -0,0 +1,14 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class MyIP {
+	public static void main(String[] args) {
+		try {
+			InetAddress local = InetAddress.getLocalHost();
+			System.out.println("내 컴퓨터 IP주소 : " + local.getHostAddress());
+			
+		}catch(UnknownHostException e) {
+			e.printStackTrace();
+		}
+	}
+}
